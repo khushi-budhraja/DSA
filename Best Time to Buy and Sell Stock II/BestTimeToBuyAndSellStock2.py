7bee0b97750e01dabb16ef6ab178043468e14ccd
khushi-budhraja <128291626+khushi-budhraja@users.noreply.github.com>
2024-01-21 13:15:30 +0530

    Add files via upload

---
+++ Best Time to Buy and Sell Stock II/BestTimeToBuyAndSellStock2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices):
+        profit = 0
+        for i in range(len(prices)-1):
+            if prices[i] < prices[i+1]:
+                profit += (prices[i+1] - prices[i])
+        return profit
+    
+prices = [7,1,5,3,6,4]
+obj = Solution()
+print(obj.maxProfit(prices))
